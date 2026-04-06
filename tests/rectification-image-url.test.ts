9e65a6902e103d18af5ed3ff9ad3d6b6f76533c2
Dev1 <davidheee@outlook.com>
2026-04-06 21:24:10 +0800

    1

--- tests/rectification-image-url.test.ts
+++ tests/rectification-image-url.test.ts
@@ -5,7 +5,7 @@ import { normalizeRectificationImageUrl } from "@/backend/rectification/rectific
 
 test("normalizeRectificationImageUrl strips OSS signature query params", () => {
   const signedUrl =
-    "https://ruipos-hyy.ruipos.com/ruipos-RLS/cos3-ri-video/202604/1775040085411-1-capture.jpg?Expires=1775126486&OSSAccessKeyId=LTAI5t86mBySpBKVbHEG2ZTa&Signature=3WOuTz5UvyVaVahFWJbnO%2BlySlA%3D";
+    "https://ruipos-hyy.ruipos.com/ruipos-RLS/cos3-ri-video/202604/1775040085411-1-capture.jpg";
 
   assert.equal(
     normalizeRectificationImageUrl(signedUrl),
